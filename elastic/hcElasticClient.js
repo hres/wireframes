1fd16aa9f9447da22d243b067c2f8412ef4ab34f
Bimal Bhagrath <bimo.17@outlook.com>
2017-08-01 12:13:24 -0400

    Create hcElasticClient.js

---
+++ elastic/hcElasticClient.js
@@ -0,0 +1,80 @@
+//hcElasticClient.js
+
+//JavaScript connection setup to elastic server
+
+var client = new elasticsearch.Client({
+  host: "https://elastic-gate.hc.local",
+  log: "trace"
+});
+
+//jQuery connection setup to elastic server
+/*
+var client = new $.es.Client({
+  hosts: "https://elastic-gate.hc.local",
+  log: "trace"
+});
+*/
+
+$(document).ready(function () {
+
+  //populate dropdown menu to current list of indexes
+
+  client.cluster.health({}, function () {
+    console.log("health");
+  })
+
+  client.cat.indices({
+    format: "json",
+  }).then(function (response) {
+
+    var disp = "";
+
+    for (var i = 0; i < response.length; i++) {
+      disp += "<option>" + response[i].index + "</option>";
+    }
+
+    $("#indexes").html(disp);
+  });
+});
+
+function perfSearch() {
+
+  var index = $("#indexes").val();
+  var str = $("#searchquery").val();
+
+  if (str.indexOf(" ") <= 0) {
+    client.search({
+      index: index,
+      body: {
+        query: {
+          match: {
+            "_all": str
+          }
+        }
+      }
+    }).then(function (response) {
+      postToGoogle(response);
+    });
+  }
+  else {
+    client.search({
+      index: index,
+      body: {
+        query: {
+          match_phrase: {
+            "_all": str
+          }
+        }
+      }
+    }).then(function (response) {
+      postToGoogle(response);
+    });
+  }
+}
+
+function postToGoogle(jsondata) {
+
+  var data = JSON.stringify(jsondata);
+
+  $("#google").html(data);
+}
