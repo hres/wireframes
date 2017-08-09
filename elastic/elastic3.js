f3bce37c812759f3078218b86d1213e15a647f2a
Bimal Bhagrath <bimo.17@outlook.com>
2017-08-09 11:03:10 -0400

    Update elastic3.js

--- elastic/elastic3.js
+++ elastic/elastic3.js
@@ -1,7 +1,20 @@
+var getQueryString = function ( field, url ) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+};
+
+var username = getQueryString('u');
+var password = getQueryString('p');
+
 function start() {
   $.ajax({
-    url: "http://elastic-gate.hc.local:80/_cat/indices?format=json",
+    url: "http://elastic-gate.hres.ca/_cat/indices?format=json",
     method: "GET",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
+    },
     success: function(response) {
       console.log(response);
     }
