b810ca90d06acc0ac99c2e4a7437a4606cd9c354
Bimal Bhagrath <bimo.17@outlook.com>
2017-08-22 08:43:47 -0400

    Update search.php

--- elastic/search.php
+++ elastic/search.php
@@ -1,6 +1,7 @@
 <?php
+
 echo "hello world";
-$file_path = "https://elastic-gate.hc.local:443/_cat/indices";
+$file_path = "https://elastic-gate.hres.ca:443/_cat/indices";
 $content = file_get_contents($file_path);
 echo $content;
  echo "<script>console.log( 'PHP: " . $content . "' );</script>"
